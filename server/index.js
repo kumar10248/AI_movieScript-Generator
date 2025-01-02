29595bf7ccd84704f7beb1b18ae67c54e70b6abd
kumar10248 <kumardevashish000@gmail.com>
2025-01-02 21:18:45 +0530

    fix: remove inappropriate language from the greeting message

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=>{
-    res.send("Hello world! chutiye 🤮🤢👄🫦");
+    res.send("Hello world!");
 });
 
 app.use("/script", require("./routes/script"));
